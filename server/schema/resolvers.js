04a47e5e766920bd87cb1695a33e0835835ecad3
baldwisj <baldwisj@gmail.com>
2024-03-13 18:32:14 -0400

    Added resolver functionality and modified server.js

---
+++ server/schema/resolvers.js
@@ -0,0 +1,68 @@
+const { User, Book } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+const { sign } = require('jsonwebtoken');
+
+const resolvers = {
+    Query: {
+        Query: {
+            me: async (parent, args, context) => {
+                if (context.user) {
+                    return await User.findOne({ _id: context.user._id });
+                }
+                throw new AuthenticationError;
+            },
+        },
+        Mutation: {
+            AddUser: async ({ username, email, password }) => {
+                const user = await User.create({ username, email, password });
+                const token = signToken(user);
+                return { token, user };
+
+            },
+            login: async (parent, { email, password }) => {
+                const user = await User.findOne({ email });
+
+                if (!user) {
+                    throw AuthenticationError;
+                }
+
+                const correctPw = await user.isCorrectPassword(password);
+
+                if (!correctPw) {
+                    throw AuthenticationError;
+                }
+
+                const token = signToken(user);
+
+                return { token, user };
+            },
+            saveBook: async (parent, { book }, context) => {
+                if (context.user) {
+                    return User.findOneAndUpdate(
+                        { id: context.user._id },
+                        { $addToSet: { savedBooks: book }},
+                        {
+                            new: true,
+                            runValidators: true
+                        },
+                    )
+                }
+                throw AuthenticationError;
+            },
+            removeBook: async (parent, { book }, context) => {
+                if (context.user) {
+                    return User.findOneAndUpdate(
+                        { _id: context.user._id },
+                        { $pull: { savedBooks: book } },
+                        {
+                            new: true,
+                            runValidators: true
+                        }
+                    )
+                }
+                throw AuthenticationError;
+            },
+        },
+    },
+};
+module.exports = resolvers;
